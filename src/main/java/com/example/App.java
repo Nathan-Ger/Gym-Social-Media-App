eb751a6f837dd939d25081da2c5f0d76257f99f1
Stallk <72036042+Stallk@users.noreply.github.com>
2024-03-10 12:02:20 -0400

    - Added Imports for MongoDB - Updated pom.xml - Added MongoDB Connection (no password within code)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -27,6 +27,33 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        
+        String dbPassword = System.getenv("MONGO_DB_PASSWORD");
+        if (dbPassword == null || dbPassword.isEmpty())
+            throw new IllegalStateException("MongoDB password not set in the environment variables.");
+
+        String connectionString = "mongodb+srv://nathanlgermain:" + dbPassword + "@befit.uxd3dmt.mongodb.net/?retryWrites=true&w=majority&appName=BeFit";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+        
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
